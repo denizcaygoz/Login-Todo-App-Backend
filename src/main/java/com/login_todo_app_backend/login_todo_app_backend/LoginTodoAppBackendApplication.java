824e1c7ba7da5365862cc2edbb697a4e1df9e2bf
Deniz Çaygöz <dnzcgz007@gmail.com>
2025-03-13 17:10:32 +0100

    first commit

---
+++ src/main/java/com/login_todo_app_backend/login_todo_app_backend/LoginTodoAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.login_todo_app_backend.login_todo_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginTodoAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginTodoAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/login_todo_app_backend/login_todo_app_backend/LoginTodoAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.login_todo_app_backend.login_todo_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginTodoAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
